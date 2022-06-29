90731b055f52e7e652a721741405f9d718f06cd5
keinaka <kei.6203@gmail.com>
2022-06-29 19:21:15 +0900

    Add GTM

--- html/cart.php
+++ html/cart.php
@@ -10,6 +10,13 @@ $added_items = $_SESSION["cart_items"];
 <html>
 
 <head>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-55K9Q2M');</script>
+  <!-- End Google Tag Manager -->
   <meta charset="utf-8">
   <title>Welcome to My Shop | Cart</title>
   <meta name="keyword" content="test-page" />
@@ -17,6 +24,10 @@ $added_items = $_SESSION["cart_items"];
 </head>
 
 <body style="margin: 0; padding: 0;">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-55K9Q2M"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
   <div data-trackable="main" style="width: 840px; margin: 40px 50px; padding: 0;">
     <header id="header" data-trackable="header" style="width: 100%; margin: 0; padding: 0;">
       <div data-trackable="title" style="float: left; height: 100px; width: 40%">
